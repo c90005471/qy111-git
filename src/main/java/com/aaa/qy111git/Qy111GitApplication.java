7596df29b185450c37ef3607fcebc29b0b170f72
teacherChen <86521760@qq.com>
2020-06-08 16:55:08 +0800

    提交人：陈建 提交原因：git演示

---
+++ src/main/java/com/aaa/qy111git/Qy111GitApplication.java
@@ -0,0 +1,16 @@
+package com.aaa.qy111git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 这是一个git测试类
+ */
+@SpringBootApplication
+public class Qy111GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qy111GitApplication.class, args);
+    }
+
+}
